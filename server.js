24f0f32ecdc1e0a5b68cb1a838f3afd52afec4d4
Aidanchase <aidanmro@gmail.com>
2019-06-20 00:55:05 +0000

    added cookie session to server.js

--- server.js
+++ server.js
@@ -7,13 +7,13 @@ const ENV         = process.env.ENV || "development";
 const express     = require("express");
 const bodyParser  = require("body-parser");
 const sass        = require("node-sass-middleware");
-const app         = express();
 const bcrypt = require("bcrypt")
 const knexConfig  = require("./knexfile");
 const knex        = require("knex")(knexConfig[ENV]);
 const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
-
+const cookieSession = require("cookie-session");
+const app         = express();
 // Seperated Routes for each Resource
 const usersRoutes = require("./routes/users");
 
