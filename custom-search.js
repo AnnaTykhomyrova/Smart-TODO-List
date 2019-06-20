9fc1fe8d7180b87888adbaf6d0a64fd1adb4c54c
Aidanchase <aidanmro@gmail.com>
2019-06-20 14:19:01 +0000

    added search engine id incase we  need it

---
+++ custom-search.js
@@ -0,0 +1,14 @@
+<script>
+  (function() {
+    var cx = '005403711920815931649:zeiceky6tfs';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:search></gcse:search>
+
+// SEARCH ENGINE ID 005403711920815931649:zeiceky6tfs
