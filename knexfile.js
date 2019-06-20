3a8d8ad318dcd1399eb2139af59c24e7dfa3176d
Aidanchase <aidanmro@gmail.com>
2019-06-20 23:32:31 +0000

    added knexfile and settings.json, also commented out useless placeholder code, updated gitignore

--- knexfile.js
+++ knexfile.js
@@ -1,35 +1,49 @@
-require('dotenv').config();
+// Update with your config settings.
+const settings = require("./settings");
 
 module.exports = {
 
   development: {
+    client: 'pg',
+    connection: {
+        user     : settings.user,
+        password : settings.password,
+        database : settings.database,
+        host     : settings.hostname,
+        port     : settings.port,
+    }
+    },
+
+
+  staging: {
     client: 'postgresql',
     connection: {
-      host     : process.env.DB_HOST,
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      ssl      : process.env.DB_SSL
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
     },
-    migrations: {
-      directory: './db/migrations',
-      tableName: 'migrations'
-    },
-    seeds: {
-      directory: './db/seeds'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL + '?ssl=true',
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'migrations'
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     }
   }
 
