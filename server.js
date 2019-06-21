8992e4f6d75ee4908f0efc3290fe589660e5349f
Aidanchase <aidanmro@gmail.com>
2019-06-21 03:20:11 +0000

    fixed spacing

--- server.js
+++ server.js
@@ -75,6 +75,14 @@ app.post("/login", (req, res) => {
 });
 
 
+app.get("/register", (req, res) => {
+  res.render("registration_page")
+});
+
+app.post ("/register", (req, res)  => {
+  res.redirect("/home")
+});
+
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
