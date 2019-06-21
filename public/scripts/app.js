fa131f3c75f4f977cbbc9f290e62db072cc47f6a
AnnaTykhomyrova <tykhomyrovaanna@gmail.com>
2019-06-21 01:18:31 +0000

    switched from master

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,4 @@
-$(() => {
+$(document).ready(function() {
   $.ajax({
     method: "GET",
     url: "/api/users"
@@ -6,5 +6,21 @@ $(() => {
     for(user of users) {
       $("<div>").text(user.name).appendTo($("body"));
     }
-  });;
+  });
+
+  $('#logout-button').on('submit',function (ev) {
+  ev.preventDefault();
+      $.ajax({
+        method: 'POST',
+        url: '/logout'
+      });
+  });
+
+  $('#update-button').on('submit',function (ev) {
+  ev.preventDefault();
+      $.ajax({
+        method: 'POST',
+        url: '/update'
+      });
+  });
 });
