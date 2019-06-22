1261538afae6bc48b2f28209f8c302223e88b171
Vishnu Chennuru <vishnuchen@gmail.com>
2019-06-22 21:43:14 +0000

    yes my api call is working

--- server.js
+++ server.js
@@ -80,9 +80,66 @@ app.get('/update', (req, res) => {
 });
 
 
-app.post('/add-item', (req, res) => {
-  res.redirect('/');
+// app.post('/add-item', (req, res) => {
+//   res.redirect('/');
+// });
+
+//for api call
+
+app.post("/add-item", (req, res) => {
+
+  let templateVars = {
+    username: req.session.username
+  };
+
+  // console.log("this is req", req.body.input)
+
+  let searchbarText = req.body.input;
+
+  const query = qs.stringify({ appid: 'X57U54-RPHVX5VRH3', input: `${searchbarText}`, output: 'json' })
+  // api.wolframalpha.com/v2/query?appid=X57U54-RPHVX5VRH3&input=harry%20potter&output=json
+
+  const apiUrl = `api.wolframalpha.com/v2/query?${query}`
+
+  request( `https://api.wolframalpha.com/v2/query?${query}` , function (error, response, body) {
+      if (error) {
+          console.log("error occured", error);
+      }
+      else if (response.statusCode === 200) {
+          //get (body);
+          var data = JSON.parse (body); 
+          let print = data.queryresult.datatypes;
+          let splitPrint =  print.split(",");
+
+          for (var i = 0; i < splitPrint.length; i++) {
+          if (splitPrint[i] === 'Book') {
+            console.log('found Books')
+          }
+          else if (splitPrint[i] === 'TelevisionProgram') {
+            console.log('found show')
+          }
+
+          else if (splitPrint[i] === 'Movie') {
+            console.log('found Movie')
+          }
+          else if (splitPrint[i] === 'ConsumerProductsPTE') {
+            console.log('found product')
+          }
+          else if (splitPrint[i] === 'RetailLocation') {
+            console.log('found restraunt or cafe')
+          }
+          else {
+            console.log('found others')
+          }
+          //Do something
+          }
+      }
+      
+  }); 
+  res.render('home_page', templateVars);
 });
+
+
 
 
 app.get("/login", (req, res) => {
@@ -173,53 +230,4 @@ app.listen(PORT, () => {
 });
 
 
-
-//for api call
-
-app.post("/add-item", (req, res) => {
-
-  let searchbarText = req.body.searchbar;
-
-  const query = qs.stringify({ appid: 'X57U54-RPHVX5VRH3', input: `${searchbarText}`, output: 'json' })
-  // api.wolframalpha.com/v2/query?appid=X57U54-RPHVX5VRH3&input=harry%20potter&output=json
-
-  const apiUrl = `api.wolframalpha.com/v2/query?${query}`
-
-  request( `api.wolframalpha.com/v2/query?${query}` , function (error, response, body) {
-      if (error) {
-          console.log("error occured", error);
-      }
-      else if (response.statusCode === 201) {
-          //get (body);
-          var data = JSON.parse (body); 
-          let print = data.queryresult.datatypes;
-          let splitPrint =  print.split(",");
-
-          for (var i = 0; i < splitPrint.length; i++) {
-          if (splitPrint[i] === 'Book') {
-            console.log('found Books')
-          }
-          else if (splitPrint[i] === 'TelevisionProgram') {
-            console.log('found show')
-          }
-
-          else if (splitPrint[i] === 'Movie') {
-            console.log('found Movie')
-          }
-          else if (splitPrint[i] === 'ConsumerProductsPTE') {
-            console.log('found product')
-          }
-          else if (splitPrint[i] === 'RetailLocation') {
-            console.log('found restraunt or cafe')
-          }
-          else {
-            console.log('found others')
-          }
-          //Do something
-          }
-      }
-      
-  }); 
-  res.redirect('/');
-});
 
