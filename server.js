d141e557554fcca133548371f0c2c98e6091c331
AnnaTykhomyrova <tykhomyrovaanna@gmail.com>
2019-06-22 19:10:20 +0000

    fix the name of the route and create containers for items

--- server.js
+++ server.js
@@ -53,7 +53,6 @@ app.get("/", (req, res) => {
     };
      return res.render("home_page", templateVars);
   }
-
   res.redirect('/login');
 });
 
@@ -66,8 +65,8 @@ app.post('/logout', (req, res) => {
 // When user click button update
 app.get('/update', (req, res) => {
   if(req.session.user_id){
-    var username = req.session.username;
-    var password = req.session.password;
+    let username = req.session.username;
+    let password = req.session.password;
     let templateVars = {
       username: username,
       password: password
@@ -79,7 +78,7 @@ app.get('/update', (req, res) => {
 });
 
 
-app.post('/update', (req, res) => {
+app.post('/add-item', (req, res) => {
   res.redirect('/');
 });
 
