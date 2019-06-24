5f3187c0d06297a6e9d1f0ee32758dd5974e8824
Aidanchase <aidanmro@gmail.com>
2019-06-24 05:45:55 +0000

    fixed master

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,7 +10,7 @@
 //   })
 // };
 //  // success: loadItems.bind(null, function (response) {
-//   // const lastItem = response[response.length - 1];
+        //   // const lastItem = response[response.length - 1];
 //         //   renderItem(`${input}`);
 
 // loadItems(function (response) {
@@ -18,66 +18,72 @@
 // });
 
 
-$(document).ready(function () {
-      $('#logout-button').on('submit', function (ev) {
-        ev.preventDefault();
+$(document).ready(function() {
+  $('#logout-button').on('submit',function (ev) {
+  ev.preventDefault();
+      $.ajax({
+        method: 'POST',
+        url: '/logout'
+      });
+  });
+
+  $('#update-button').on('submit',function (ev) {
+  ev.preventDefault();
+      $.ajax({
+        method: 'POST',
+        url: '/update'
+      });
+  });
+  
+  $('#form').on('submit',function (ev) {
+    ev.preventDefault();
+    var input = $('input.search-bar').val();
+    $('#form input').val('');
         $.ajax({
           method: 'POST',
-          url: '/logout'
-        });
-      });
-
-      $('#update-button').on('submit', function (ev) {
-        ev.preventDefault();
-        $.ajax({
-          method: 'POST',
-          url: '/update'
-        });
-      });
-
-      $('input:checkbox').on('change', function () {
-        var input = $(this).next('li');
-        if (this.checked) {
-          $(input).css('textDecoration', 'line-through');
-        } else {
-          $(input).css('textDecoration', 'none');
-        }
-      });
-        })
-        $("#form").on('submit', function (ev) {
-          ev.preventDefault();
-          var input = $('input.search-bar').val();
-          $('#form input').val('');
-          $.ajax({
-            method: 'POST',
-            url: '/add-item',
-            data: {
-              input
-            },
-            success: function (response) {
-              console.log(response);
-            }
-          })
-
-          $.ajax({
-            method: 'GET',
-            url: '/get-item',
-            data: JSON,
-            success: function (response) {
-              for (let i in response) {
-                if (response[i].api_response === "books") {
-                  $("#read-container").append(`<li> ${input}</li>`)
-                } else if (response[i].api_response === "films") {
-                  $("#read-container").append(`<li> ${input}</li>`)
-                } else if (response[i].api_response === "restaurants") {
-                  $("#read-container").append(`<li> ${input}</li>`)
-                } else if (response[i].api_response === "products") {
-                  $("#read-container").append(`<li> ${input}</li>`)
-                } else if (response[i].api_response === "other") {
-                  $("#read-container").append(`<li> ${input}</li>`)
-                }
+          url: '/add-item',
+          data: {input},
+          success: function(response) {
+            console.log(response);
+          }
+         })
+     
+         $.ajax({
+        method: 'GET',
+        url: '/get-item',
+        data: JSON,
+        success: function (response){
+          for (let i in response){
+            console.log(response[i].api_response)
+            if (response[i].api_response === "books"){
+              console.log('I got a book')
+              // $("#read-container").append(`<div> ${input}</div>`)
+            } 
+                else if (response[i].api_response === "films"){
+              // $("#read-container").append(`<div> ${input}</div>`)
+              console.log('I got a movie')
+           } 
+                else if (response[i].api_response === "restaurants"){
+                  // $("#read-container").append(`<div> ${input}</div>`)
+                  console.log('I got a resto')
+               }
+                else if (response[i].api_response === "products"){
+              // $("#read-container").append(`<div> ${input}</div>`)
+              console.log('I got a thhingh')
+               } 
+               else if (response[i].api_response === "other"){
+              // $("#read-container").append(`<div> ${input}</div>`)
+              console.log('I got a other')
+               }
               }
-            }
-          })
+          }
         })
+      })
+    })
+  // $('#form').on('submit',function (ev) {
+  //   ev.preventDefault();
+  //   var input = $('#form input').val();
+  //   let newItem = $(`<div> ${input} </div>`)
+  //   $('').preppend(newItem);
+  // })
 
