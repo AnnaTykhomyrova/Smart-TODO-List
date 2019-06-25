2e89da7250ef1d8cc5e7cd6b880c4681b4dc3a6d
AnnaTykhomyrova <tykhomyrovaanna@gmail.com>
2019-06-25 19:43:40 +0000

    working master branch with containers for categories and delete, change category buttons

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,31 +4,41 @@ function loadItems(callback) { //$jquery/ ajax request to load new tweets onto p
     url: '/get-item',
     data: JSON,
     success: callback
-  })
-};
+  });
+}
 
 loadItems(function (response) {
-  response.forEach((item) => renderItems(item))
+  response.forEach((item) => renderItems(item));
 });
 
 function renderItems(item) {
-    if (item.category_id === 1){
-        $('.read-container').append(`<ul> <li>${item.user_description}</li></ul>`)
-      } 
-          else if (item.category_id === 2){
+if (item.category_id === 1){
+    $('.read-container').append(`<ul><li>- ${item.user_description}</li><div class="forms-container"><form class="delete-forms">
+      <button type='submit'>Delete</button></form><form method="GET" action="/change-category" class="change-category-forms">
+      <button type='submit'>Change a category</button></form></div></ul>`);
+  }
+      else if (item.category_id === 2){
 
-            $('.watch-container').append(`<ul> <li>${item.user_description}</li></ul>`)
-     } 
-          else if (item.category_id === 3){
+        $('.watch-container').append(`<ul><li>- ${item.user_description}</li><div class="forms-container"><form class="delete-forms">
+      <button type='submit'>Delete</button></form><form method="GET" action="/change-category" class="change-category-forms">
+      <button type='submit'>Change a category</button></form></div></ul>`);
+ }
+      else if (item.category_id === 3){
 
-            $('.buy-container').append(`<ul> <li>${item.user_description}</li></ul>`)
-         } 
-         else if (item.category_id === 4){
-          $('.eat-container').append(`<ul> <li>${item.user_description}</li></ul>`)
-       }
-         else if (item.category_id === 5){
-          $('.other-container').append(`<ul> <li>${item.user_description}</li></ul>`)
-         }
+        $('.buy-container').append(`<ul><li>- ${item.user_description}</li><div class="forms-container"><form class="delete-forms">
+      <button type='submit'>Delete</button></form><form method="GET" action="/change-category" class="change-category-forms">
+      <button type='submit'>Change a category</button></form></div></ul>`);
+     }
+     else if (item.category_id === 4){
+      $('.eat-container').append(`<ul><li>- ${item.user_description}</li><div class="forms-container"><form class="delete-forms">
+      <button type='submit'>Delete</button></form><form method="GET" action="/change-category" class="change-category-forms">
+      <button type='submit'>Change a category</button></form></div></ul>`);
+   }
+     else if (item.category_id === 5){
+      $('.other-container').append(`<ul><li>- ${item.user_description}</li><div class="forms-container"><form class="delete-forms">
+      <button type='submit'>Delete</button></form><form method="GET" action="/change-category" class="change-category-forms">
+      <button type='submit'>Change a category</button></form></div></ul>`);
+     }
 }
 
 $(document).ready(function() {
@@ -48,7 +58,8 @@ $(document).ready(function() {
         url: '/update'
       });
   });
-  
+
+
   $('#form').on('submit',function (ev) {
     ev.preventDefault();
       input = $('input.search-bar').val();
@@ -60,8 +71,8 @@ $(document).ready(function() {
           success: function(response) {
             console.log(response);
           }
-         })
-     
+         });
+
          $.ajax({
         method: 'GET',
         url: '/get-item',
@@ -71,25 +82,25 @@ $(document).ready(function() {
           const lastItem = response[response.length - 1];
           renderItems(lastItem);
         })
-        
-      })
+
+      });
     });
-  })  
-        
-        
-        
-        
-        
-        
-        
-        
-        
+  });
+
+
+
+
+
+
+
+
+
         //function (response){
         //    input = $('#form input.search-bar').val();
         //   for (let i in response){
-           
+
         //     switch (response[i].category_id){
-        //       case 1: 
+        //       case 1:
         //       $('.read-container').append(`<ul> <li>${response[i].user_description}</li></ul>`)
         //     break;
 
@@ -100,35 +111,35 @@ $(document).ready(function() {
         //           case 3:
         //           $('.buy-container').append(`<ul> <li>${response[i].user_description}</li></ul>`)
         //           break;
-                  
+
         //           case 4:
         //         $('.eat-container').append(`<ul> <li>${response[i].user_description}</li></ul>`)
-                
+
         //           break;
-                 
+
         //           case 5:
         //         $('.other-container').append(`<ul> <li>${response[i].user_description}</li></ul>`)
         //           break;
         //     }
-           
+
         //   }
         // }
-     
+
             // console.log(response[i].category_id)
           //   if (response[i].category_id === 1){
           //     $('.read-container').append(`<ul> <li>${response[i].user_description}</li></ul>`)
-          //   } 
+          //   }
           //       else if (response[i].category_id === 2){
 
           //         $('.watch-container').append(`<ul> <li>${response[i].user_description}</li></ul>`)
-          //  } 
+          //  }
           //       else if (response[i].category_id === 3){
           //         $('.eat-container').append(`<ul> <li>${response[i].user_description}</li></ul>`)
           //      }
           //       else if (response[i].category_id === 4){
 
           //         $('.buy-container').append(`<ul> <li>${response[i].user_description}</li></ul>`)
-          //      } 
+          //      }
           //      else if (response[i].category_id === 5){
           //       $('.other-container').append(`<ul> <li>${response[i].user_description}</li></ul>`)
           //      }
@@ -142,4 +153,3 @@ $(document).ready(function() {
   //   var input = $('#form input').val();
   //   let newItem = $(`<div> ${input} </div>`)
   //   $('').preppend(newItem);
-  
